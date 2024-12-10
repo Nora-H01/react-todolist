ff3655c2ff38559de5876ada0c26b1ff639fd216
Nora-H1 <nora.hashani01@gmail.com>
2024-12-10 16:04:05 +0100

    update ToDOList.jsx : add className state-indicator & option value

--- react-todolist/src/ToDoList.jsx
+++ react-todolist/src/ToDoList.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import "./style.css";
 
-const TodoList = ({ todos, toggleTodo }) => {
+const TodoList = ({ todos, updateTodoState }) => {
   return (
     <div className="todo-list-container">
       <h2>Todos</h2>
       <ul className="todo-list">
         {todos.map((todo) => (
-          <li key={todo.id} className={`todo-item ${todo.done ? "done" : ""}`}>
-            <input
-              type="checkbox"
-              id={todo.id}
-              name={todo.name}
-              checked={todo.done}
-              onChange={() => toggleTodo(todo.id)}
-              className="todo-checkbox"
-            />
-            {todo.name}
+          <li key={todo.id} className={`todo-item ${todo.state}`}>
+            <div className="todo-item-content">
+              <span
+                className="state-indicator"
+                style={{
+                  backgroundColor:
+                    todo.state === "done"
+                      ? "green"
+                      : todo.state === "in progress"
+                      ? "orange"
+                      : "red",
+                }}
+              ></span>
+              {todo.name}
+            </div>
+            <select
+              value={todo.state}
+              onChange={(e) => updateTodoState(todo.id, e.target.value)}
+              className="state-selector"
+            >
+              <option value="to do">To Do</option>
+              <option value="in progress">In Progress</option>
+              <option value="done">Done</option>
+            </select>
           </li>
         ))}
       </ul>
