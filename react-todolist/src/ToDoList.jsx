6b68b7ac6fbeb6d88210f1460191b4ef06e13ab7
Nora-H1 <nora.hashani01@gmail.com>
2024-12-16 11:03:17 +0100

    deplace todo-counter & add category ToDoList.jsx

--- react-todolist/src/ToDoList.jsx
+++ react-todolist/src/ToDoList.jsx
@@ -1,44 +1,57 @@
-import React from "react";
-import "./style.css";
-
 const TodoList = ({ todos, updateTodoState, deleteTodo, deleteAllCompleted }) => {
+  const privateTodos = todos.filter((todo) => todo.category === "private");
+  const workTodos = todos.filter((todo) => todo.category === "work");
+
+   //counter
+   const getIncompleteCount = () => {
+    return todos.filter((todo) => todo.state !== "done").length;
+  };
+
+  const renderTodoList = (todoList) =>
+    todoList.map((todo) => (
+      <li key={todo.id} className={`todo-item ${todo.state}`}>
+        <div className="todo-item-content">
+          <span
+            className={`state-indicator ${
+              todo.state === "done"
+                ? "state-done"
+                : todo.state === "in progress"
+                ? "state-in-progress"
+                : "state-to-do"
+            }`}
+          ></span>
+          {todo.name}
+        </div>
+        <select
+          value={todo.state}
+          onChange={(e) => updateTodoState(todo.id, e.target.value)}
+          className="state-selector"
+        >
+          <option value="to do">To Do</option>
+          <option value="in progress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+        {todo.state === "done" && (
+          <button onClick={() => deleteTodo(todo.id)} className="delete-btn">
+            X
+          </button>
+        )}
+      </li>
+    ));
+
   return (
     <div className="todo-list-container">
-      <h2>Todos</h2>
-      <ul className="todo-list">
-        {todos.map((todo) => (
-          <li key={todo.id} className={`todo-item ${todo.state}`}>
-            <div className="todo-item-content">
-              <span
-                className={`state-indicator ${
-                  todo.state === "done"
-                    ? "state-done"
-                    : todo.state === "in progress"
-                    ? "state-in-progress"
-                    : "state-to-do"
-                }`}
-              ></span>
-              {todo.name}
-            </div>
-            <select
-              value={todo.state}
-              onChange={(e) => updateTodoState(todo.id, e.target.value)}
-              className="state-selector"
-            >
-              <option value="to do">To Do</option>
-              <option value="in progress">In Progress</option>
-              <option value="done">Done</option>
-            </select>
-            {/* Display the delete button if the task has been completed */}
-            {todo.state === "done" && (
-              <button onClick={() => deleteTodo(todo.id)} className="delete-btn">
-                X
-              </button>
-            )}
-          </li>
-        ))}
-      </ul>
-      {/* Button to delete all completed tasks */}
+      <h4>-------------------------------</h4>
+      <h2>Todo List</h2>
+      <p className="todo-counter">{getIncompleteCount()} todos left</p>
+      <div className="category">
+        {/* <h3>Private</h3> */}
+        <ul className="todo-list">{renderTodoList(privateTodos)}</ul>
+      </div>
+      <div className="category">
+        {/* <h3>Work</h3> */}
+        <ul className="todo-list">{renderTodoList(workTodos)}</ul>
+      </div>
       <button onClick={deleteAllCompleted} className="delete-all-btn">
         <span>X</span> All
       </button>
