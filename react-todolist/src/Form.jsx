5a84d72c482e08fb413228431cd1e3a9fb01dfc0
Nora-H1 <nora.hashani01@gmail.com>
2024-12-20 09:39:35 +0100

    Update : add calendar button & show date Form.jsxx

--- react-todolist/src/Form.jsx
+++ react-todolist/src/Form.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
+import CalendarComponent from "./calendar";
 
 const Form = ({ addTodo, activeTab }) => {
   const [taskName, setTaskName] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null); // Stocker la date sélectionnée
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (taskName.trim()) {
-      addTodo(taskName, activeTab); // Utilise directement `activeTab` pour la catégorie
-      setTaskName(""); // Réinitialiser l'input
+      addTodo(taskName, activeTab, selectedDate); // Ajouter la date à la tâche
+      setTaskName("");
+      setSelectedDate(null);
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="todo-form">
+        {/* add calendar button */}
+        <CalendarComponent onDateSelect={setSelectedDate} />
+  
+        {/* show date */}
+        {selectedDate && (
+          <p className="selected-date">
+            Selected date : {selectedDate.toLocaleDateString()}
+          </p>
+        )}
+        
       <input
         type="text"
         value={taskName}
@@ -20,6 +33,8 @@ const Form = ({ addTodo, activeTab }) => {
         placeholder="Enter a task..."
         className="task-input"
       />
+
+
       <button type="submit" className="add-button">
         Add
       </button>
